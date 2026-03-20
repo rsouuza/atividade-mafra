2f277fc781acfcfd638cfbebe7c7e3bc5e2cfc10
rsouuza <raulviniciusdesouza@gmail.com>
2026-03-20 20:18:57 -0300

    acertando

--- src/main/java/faculdade/teste/TesteApplication.java
+++ src/main/java/faculdade/teste/TesteApplication.java
@@ -2,9 +2,28 @@ package faculdade.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class TesteApplication {
+
+	@PostMapping("/contato")
+	public String criarContato() {
+		return "Contato criado com sucesso!";
+	}
+	@RequestMapping("/aqui")
+	public String puxarContato() {
+		return "O contato está aqui.";
+	}
+	@DeleteMapping("/deletar")
+	public String deletarContato() {
+		return "Você deletou o contato.";
+	}
+	@PutMapping("/postar")
+	public String consultarCtt(){
+		return "Você consultou o contato.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteApplication.class, args);
